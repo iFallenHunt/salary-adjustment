a56c37aba55c5d835b9f7fab384f7fdf7d8db726
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-05-26 16:09:56 -0300

    how to calculate the salary adjustment

---
+++ readjustment.py
@@ -0,0 +1,32 @@
+job = int(input('Type the title: '))
+salary = float(input('Type salary: '))
+readjustment = float
+increase = float
+
+if job == 1:
+  print('General Services')
+  increase = salary * (50/100)
+  readjustment = salary + increase
+  print('The raise is: ', increase)
+  print('The new salary is: ', readjustment)
+else:
+  if job == 2:
+    print('Watchman')
+    increase = salary * (30/100)
+    readjustment = salary + increase
+    print('The raise is: ', increase)
+    print('The new salary is: ', readjustment)
+  else:
+    if job == 3:
+      print('Receptionist')
+      increase = salary * (25/100)
+      readjustment = salary + increase
+      print('The raise is: ', increase)
+      print('The new salary is: ', readjustment)
+    else:
+      if job == 4:
+        print('Vendecor')
+        increase = salary * (15/100)
+        readjustment = salary + increase
+        print('The raise is: ', increase)
+        print('The new salary is: ', readjustment)
